f74719eabe5b689dae7dbb05611243d46febfd63
praveenk222 <praveenkadma222@gmail.com>
2023-12-14 18:23:39 +0530

    latest

--- src/app/E-bike-booking-flow/duration/duration.component.ts
+++ src/app/E-bike-booking-flow/duration/duration.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import {MatBottomSheet, MatBottomSheetModule} from '@angular/material/bottom-sheet';
+import { MatBottomSheet, MatBottomSheetModule } from '@angular/material/bottom-sheet';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute, Router } from '@angular/router';
 import { BookingService } from 'src/app/E-booking-flow-services/booking.service';
@@ -15,84 +15,66 @@ interface Food {
   templateUrl: './duration.component.html',
   styleUrls: ['./duration.component.scss'],
 })
-export class DurationComponent  implements OnInit {
-  customDate!:FormGroup;
-  productId:any;
-  ProductDetails:any
+export class DurationComponent implements OnInit {
+  customDate!: FormGroup;
+  productId: any;
+  ProductDetails: any
 
-  taskId:any; 
-<<<<<<< HEAD
+  taskId: any;
   selectedValue!: string;
   selectedValue1!: string;
 
   foods: Food[] = [
-    {value: '1', viewValue: '1'},
-    {value: '2', viewValue: '2'},
-    {value: '3', viewValue: '3'},
-    {value: '4', viewValue: '4'},
-    {value: '5', viewValue: '5'},
-    {value: '6', viewValue: '6'},
+    { value: '1', viewValue: '1' },
+    { value: '2', viewValue: '2' },
+    { value: '3', viewValue: '3' },
+    { value: '4', viewValue: '4' },
+    { value: '5', viewValue: '5' },
+    { value: '6', viewValue: '6' },
   ];
   foods1: Food[] = [
-    {value: 'hours', viewValue: 'hours'},
-    {value: 'days', viewValue: 'days'},
-    {value: 'weeks', viewValue: 'weeks'},
+    { value: 'hours', viewValue: 'hours' },
+    { value: 'days', viewValue: 'days' },
+    { value: 'weeks', viewValue: 'weeks' },
 
   ];
-  constructor(private snackBar: MatSnackBar, private router:Router,private booking:BookingService,private bk:FormBuilder,private route: ActivatedRoute,private user:UserData) {
-=======
-  constructor(private snackBar: MatSnackBar, private router:Router,private bookingservice:BookingService,private bk:FormBuilder,private route: ActivatedRoute,private user:UserData) {
->>>>>>> ede13e1c5599b07525450bcf5b240170d9cec95e
-    this.customDate=this.bk.group({
-      date:['',Validators.required],
-      time:['',Validators.required]
+  constructor(private snackBar: MatSnackBar, private router: Router, private bookingservice: BookingService, private bk: FormBuilder, private route: ActivatedRoute, private user: UserData) {
+    this.customDate = this.bk.group({
+      date: ['', Validators.required],
+      time: ['', Validators.required]
     })
   }
 
   toppings = new FormControl('');
 
-  toppingList: string[] = ['hours','days','weeks'];
+  toppingList: string[] = ['hours', 'days', 'weeks'];
   toppings1 = new FormControl('');
 
-  toppingList1: string[] = ['1','2','3','4','5','6','7','8'];
+  toppingList1: string[] = ['1', '2', '3', '4', '5', '6', '7', '8'];
   ngOnInit() {
-    this.user.getId('pId').then(data => this.productId=data);  
+    this.user.getId('pId').then(data => this.productId = data);
 
   }
-  duration(){
+  duration() {
     const data = this.customDate.value;
     console.log(this.customDate.value)
   }
-  book(){
+  book() {
     const data = this.customDate.value;
-    if(!this.customDate.valid) {
+    if (!this.customDate.valid) {
       this.customDate.markAllAsTouched();
       this.snackBar.open(" All fields are required ");
-    }else{
-    console.log(this.customDate.value)
-    this.bookingservice.book(this.productId).subscribe(
-      (res:any)=>{
-      this.ProductDetails=res
-<<<<<<< HEAD
-      this.snackBar.open(JSON.stringify(res.message)
-      );
-      this.router.navigate(['/booking-summary/1000'])
-      this.snackBar.open(JSON.stringify('Booked successfully'));
-      
-    })
-   
-  
-  }
- 
-        
-=======
-      this.snackBar.open(JSON.stringify(res.message));
-      // this.snackBar.open(JSON.stringify('Booked successfully'));
-      this.router.navigateByUrl('/booking-summary');
+    } else {
+      console.log(this.customDate.value)
+      this.bookingservice.book(this.productId).subscribe(
+        (res: any) => {
+          this.ProductDetails = res
+          this.snackBar.open(JSON.stringify(res.message));
+          // this.snackBar.open(JSON.stringify('Booked successfully'));
+          this.router.navigateByUrl('/booking-summary');
+        }
+      )
     }
-    )
->>>>>>> ede13e1c5599b07525450bcf5b240170d9cec95e
-      }
-     
+  }
 
 }
