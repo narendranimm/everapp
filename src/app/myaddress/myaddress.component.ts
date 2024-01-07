d999960b1b13ba4e572988707049736f46d0d8dc
Narendra Nimmalapalli <narendra@evertaxi.online>
2024-01-07 08:40:27 +0530

    code

--- src/app/myaddress/myaddress.component.ts
+++ src/app/myaddress/myaddress.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { RegisterService } from '../registration-services/register.service';
+import { ActivatedRoute } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-myaddress',
@@ -6,9 +9,73 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./myaddress.component.scss'],
 })
 export class MyaddressComponent  implements OnInit {
+data:any;
+address:any;
+addressid:any;
+  constructor(private _ad:RegisterService,private route: ActivatedRoute,public alertController: AlertController) { 
+    this.addressid = route.snapshot.params["ID"];
+    console.log("this is branchid value = " + this.addressid);
+  }
 
-  constructor() { }
+  ngOnInit() {
 
-  ngOnInit() {}
+    this.getAddress()
+  }
+getAddress(){
+  this._ad.getaddress(this.data).subscribe((res)=>{
+    this.data=res
+   console.log(res)
+  })
+}
+
+alertButtons = ['Action'];
+
+// showAlert() {
+
+//   this.alertController.create({
+//     header: 'Delete address',
+//     subHeader: 'Are you sure you want to delete this address?',
+//     message: 'This is an alert message.',
+//     img:'',
+//     buttons: ['OK']
+//   }).then(res => {
+
+//     res.present();
+
+//   });
+
+// }
+
+async presentDeleteAlert() {
+  const alert = await this.alertController.create({
+    header: 'Confirm Delete',
+    message: 'Are you sure you want to delete this data?',
+    buttons: [
+      {
+        text: 'Cancel',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        },
+      },
+      {
+        text: 'Delete',
+        handler: () => {
+          console.log('Delete clicked');
+          this.delterecrd()
+          // Call your delete method or perform the deletion here
+        },
+      },
+    ],
+  });
+
+  await alert.present();
+}
+  delterecrd() {
+    this._ad.deleteaddress(this.data).subscribe((res)=>{
+        console.log(res)
+    })
+  }
+
 
 }
