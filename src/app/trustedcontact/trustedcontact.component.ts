99ef4fafc0eb419e95ed82804d7d54fba562a8d5
Narendra Nimmalapalli <narendra@evertaxi.online>
2024-01-10 00:54:58 +0530

    code

--- src/app/trustedcontact/trustedcontact.component.ts
+++ src/app/trustedcontact/trustedcontact.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { RegisterService } from '../registration-services/register.service';
+import { UserData } from '../providers/user-data';
+import { ActivatedRoute, Router } from '@angular/router';
+import { SnackbarService } from '../snackbar.service';
+import { LoadingService } from '../services/loading.service';
 
 @Component({
   selector: 'app-trustedcontact',
@@ -8,7 +13,9 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class TrustedcontactComponent  implements OnInit {
    trustedForm!:FormGroup
-  constructor(private _rf:FormBuilder) { 
+  constructor(private _rf:FormBuilder,private userdata: UserData ,private router:Router,
+    private _ad:RegisterService,private route:ActivatedRoute,private loadingservice:LoadingService,
+    private snackbarService: SnackbarService) { 
     this.trustedForm=this._rf.group({
         fname:'',
         lname:'',
@@ -17,5 +24,12 @@ export class TrustedcontactComponent  implements OnInit {
     }
 
     ngOnInit() {}
+
+    savecontact(){
+        const data=this.trustedForm.value;
+        console.log(data)
+       
+    }
+
 
   }
