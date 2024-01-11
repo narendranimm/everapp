358047c2fabfe7c4cbcbe7f9cc0eba9fd127c935
praveenk222 <praveenkadma222@gmail.com>
2024-01-11 17:24:30 +0530

    dd

--- src/app/E-bike-booking-flow/paymentgateways/paymentgateways.component.ts
+++ src/app/E-bike-booking-flow/paymentgateways/paymentgateways.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-paymentgateways',
@@ -6,8 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./paymentgateways.component.scss'],
 })
 export class PaymentgatewaysComponent  implements OnInit {
+type:string=''
+  constructor(private route:ActivatedRoute) { 
 
-  constructor() { }
+const id = route.snapshot.params["ID"];;
+
+    switch (id) {
+      case 100:
+        this.type='bike'
+        
+        break;
+    
+      case 200:
+        this.type='battery'
+        
+        break;
+    
+      case 300:
+        this.type='wash'
+        
+        break;
+    
+      default:
+        break;
+    }
+  }
 
   ngOnInit() {}
 
--- src/app/batter-reservation-flow/booking-details/booking-details.component.ts
+++ src/app/batter-reservation-flow/booking-details/booking-details.component.ts
@@ -1,8 +1,12 @@
+import { DatePipe } from '@angular/common';
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute, Router } from '@angular/router';
-import { IonContent } from '@ionic/angular';
+import { IonContent, ModalController } from '@ionic/angular';
 import { ProductServicesService } from 'services/product-services/product-services.service';
 import { BookingService } from 'src/app/E-booking-flow-services/booking.service';
+import { DataservicesService } from 'src/app/dataservices.service';
 import { UserData } from 'src/app/providers/user-data';
 import { LoadingService } from 'src/app/services/loading.service';
 import { environment } from 'src/environments/environment.prod';
@@ -19,17 +23,19 @@ export class BookingDetailsComponent  implements OnInit {
   @ViewChild(IonContent) content!: IonContent;
   azimageUrl:any=environment.azimageUrl_hub;
   imageUrl:any;
+  BookingID: any;
 
- constructor(private route: ActivatedRoute,private loader:LoadingService,
-   private _pd:ProductServicesService,private router:Router,private store:UserData,
-   private _bh:BookingService) {
-    this.store.getId('hubid').then(data=>{
+  constructor( private snackBar: MatSnackBar,public dialog: MatDialog,
+    private loader:LoadingService,public modalController: ModalController,private _ps:ProductServicesService,
+     private router: Router, private bookingservice: BookingService,
+      private route: ActivatedRoute, private user: UserData,) {
+    this.user.getId('hubid').then(data=>{
       if(data !== null){
         this.payload.HubID=data;
 
       }
     })
-    this.store.getId('pId').then(data=>{
+    this.user.getId('pId').then(data=>{
       if(data !== null){
         this.productID=data;
         this.payload.ProductID=data;
@@ -48,15 +54,16 @@ ProductDetails:any;
  }
 
  getDetails(){
-  //  this.loader.simpleLoader('Loading...')
+   this.loader.simpleLoader('Loading...')
    //get details by product id and hubid
-   this._pd.getdatailsByPIDNdHubId(this.payload).subscribe(
-    (res)=>{
+   this._ps.getdatailsByPIDNdHubId(this.payload).subscribe(
+    (res:any)=>{
       if(res){
 
         this.ProductDetails=res;
         console.log(res)
         this.imageUrl=this.azimageUrl+this.ProductDetails.ImageName;
+        this.SaveOrder();
       }
      this.loader.dismissLoader();
    },
@@ -81,8 +88,88 @@ ProductDetails:any;
  }
  payload={
   
-    "ProductID":'NULL',
-    "HubID":"NULL"
+    "ProductID":0,
+    "HubID":0
 
  }
+SaveOrder(){
+    this.loader.simpleLoader('Loading...')
+    this.ordersaveData.ProductID = this.payload.ProductID;
+    this.ordersaveData.BookingStartDate =null;
+    this.ordersaveData.BookingEndDate = null;
+    this.ordersaveData.SecurityAmount=0
+    this.ordersaveData.WashAmount=0
+    this.ordersaveData.BookingAmount =0
+    this.ordersaveData.AdvanceAmount=0
+    this.ordersaveData.PaidAmount=0;
+    this.ordersaveData.TotalAmount=0;
+    this.ordersaveData.HubID=this.payload.HubID;
+    if(this.productID == null ){
+      this.snackBar.open("Please Select a Product")
+    this.loader.dismissLoader();
+      return;
+    }
+
+
+    this.bookingservice.book(this.ordersaveData).subscribe(
+      (res: any) => {
+       if(res ){
+
+         this.loader.dismissLoader();
+if(!res.Id){
+ this.snackBar.open('booking failed');
+
 }
+         this.BookingID = res.ID
+         this.user.setNew('bookingNo',this.BookingID)
+        //  this.user.setNew('startTime',null)
+        //  this.user.setNew('endTime',null)
+         this.snackBar.open(JSON.stringify(res.message));
+         this.router.navigateByUrl('/pay/'+this.BookingID);
+       }else{
+         this.loader.dismissLoader();
+       }
+       // this.dialog.open(CompletekycComponent);
+       // this.router.navigateByUrl('/adhar');
+      },
+      (error)=>{
+        this.loader.dismissLoader();
+        this.snackBar.open('booking failed');
+        
+      }
+    )
+}
+  //#region dummy data
+  ordersaveData = {
+    "OrderID": 123,
+    "ProductID": 0,
+    "BookingStartDate": null,
+    "BookingEndDate": null,
+    "IsActive": true,
+    "BookingNo": "ABC123",
+    "HubID": 0,
+    "MemberID": 0,
+    "BookingStatus": 2,
+    "AddressID": 0,
+    "BookingAmount": 0,
+    "AdvanceAmount": 0,
+    "DiscountAmount": 0,
+    "TaxAmount": 0,
+    "TotalAmount": 0,
+    "PaidAmount": 0,
+    "IsCancel": false,
+    "Remarks": "Bike Booking",
+    "CreatedOn": "2023-11-28T00:30:42",
+    "DeliveredOn": "2023-11-30T00:30:42",
+    "PaymentConfirmedOn": null,
+    "IsFullPaid":0,
+    "WashAmount":0,        
+    "SecurityAmount":0,
+    "CouponID":0,
+    "ServiceType":3508
+  }
+}
+
+//bike=3507
+//btry=3508
+//wash=3506
