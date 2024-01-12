491cd03a433b6d77a440d0d1915d1595c2dd8a12
Narendra Nimmalapalli <narendra@evertaxi.online>
2024-01-12 12:47:31 +0530

    code

--- src/app/menu-flow/settings/settings.component.ts
+++ src/app/menu-flow/settings/settings.component.ts
@@ -77,12 +77,43 @@ remove(){
   }
   )
 
+  const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
+
+  // Initialize the dark theme based on the initial
+  // value of the prefers-color-scheme media query
+  this.initializeDarkTheme(prefersDark.matches);
+
+  // Listen for changes to the prefers-color-scheme media query
+  prefersDark.addEventListener('change', (mediaQuery) => this.initializeDarkTheme(mediaQuery.matches));
+}
+
+// Check/uncheck the toggle and update the theme based on isDark
+initializeDarkTheme(isDark:any) {
+  this.themeToggle = isDark;
+  this.toggleDarkTheme(isDark);
+}
+
+// Listen for the toggle check/uncheck to toggle the dark theme
+toggleChange(ev:any) {
+  this.toggleDarkTheme(ev.detail.checked);
+}
+
+// Add or remove the "dark" class on the document body
+toggleDarkTheme(shouldAdd:any) {
+  document.body.classList.toggle('dark', shouldAdd);
+
+
 }
   ///csss
  toggleControl = new FormControl(false);
   @HostBinding('class')  className = '';
  darkClassName = 'theme-dark';
  lightClassName = 'theme-light';
+
+
+ themeToggle = false;
+
+
 }
 
 
